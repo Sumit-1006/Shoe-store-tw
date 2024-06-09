fb46dedd2981e194908871a587e398c171b0b26e
Sumit-1006 <skkatiyar10603@gmail.com>
2024-06-09 20:50:53 +0530

    All components added

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -1,3 +1,4 @@
+import PopularProductCard from "../components/PopularProductCard"
 import { products } from "../constants"
 
 const PopularProduct = () => {
@@ -10,6 +11,15 @@ const PopularProduct = () => {
         <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
           Experience top notch quality and style with our sought-after selections. Discover a world of comfort, design and value.
         </p>
+
+        <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+          {products.map((product)=>(
+            <PopularProductCard key=
+            {product.name} {...product} />
+          ))
+          }
+
+        </div>
 
       </div>
 
